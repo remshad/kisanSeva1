0d90b8cd34a84f41c73892b9179a665000e4e056
jeevan371 <knightrider2651rko@gmail.com>
2020-08-23 17:41:12 +0530

    Create loadcrops.php

---
+++ ajax/loadcrops.php
@@ -0,0 +1,12 @@
+<?php
+require_once('dbs.php'); 
+$c_id=$_GET['c_id'];
+$sql = "SELECT * FROM crop WHERE cc_id=$c_id";	
+$result = mysqli_query($link, $sql);
+if (mysqli_num_rows($result) > 0) {    
+	while($row = mysqli_fetch_assoc($result)) {
+		echo "<option value='".$row["c_id"]. "'>".$row["c_name"]. "</option>";	
+	}
+}
+mysqli_close($conn);
+?>
