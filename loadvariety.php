a3020a2ca8b435086c34bd221a503db68c3a9acd
jayaram1095 <57343892+jayaram1095@users.noreply.github.com>
2020-08-29 14:25:30 +0530

    variety

--- loadvariety.php
+++ loadvariety.php
@@ -5,7 +5,7 @@ $sql = "SELECT * FROM variety WHERE c_id=$c_id";
 $result = mysqli_query($link, $sql);
 if (mysqli_num_rows($result) > 0) {    
 	while($row = mysqli_fetch_assoc($result)) {
-		echo "<option value='".$row["v_id"]. "'>".$row["v_name"]. "</option>";	
+		echo "<option value='".$row["cv_id"]. "'>".$row["v_name"]. "</option>";	
 	}
 }
 mysqli_close($conn);
